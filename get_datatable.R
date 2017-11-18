238114c2b60805bc04caaaa918cafb0507fa4fe3
agvelazquez90 <angel.velazquez@upr.edu>
2017-11-17 20:28:57 -0400

    Update get_datatable.R

--- get_datatable.R
+++ get_datatable.R
@@ -1,6 +1,9 @@
+# Get table from internet using rvest
 library(rvest)
 page <-  read_html(" URL ")
 
 listings <- html_nodes(page, "table")
 df.table <- as.data.frame(html_table(listings))
+
+#this is to write a cvs
 write.csv(df.table, file = "datos.csv")
