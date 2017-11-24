16aee3b4f75882f7e8b31e62c99be7570d85c514
agvelazquez90 <angel.velazquez@upr.edu>
2017-11-24 17:05:30 -0400

    Update and rename proyecciones.R to projections_HoltWinters.R

--- projections_HoltWinters.R
+++ projections_HoltWinters.R
@@ -1,12 +1,11 @@
-ts_projection <- function(data, star.date, frequency , interval, titulo){
+ts_projection <- function(data, star.date, frequency , interval, title){
   library(ggplot2)
   library(ggfortify)
   library(forecast)
   df.ts = ts(data ,start=star.date, frequency = frequency)
   ts.hw <- HoltWinters(df.ts, gamma = FALSE)
   projection <- forecast(ts.hw , level = c(95), h = interval) 
-  autoplot( projection, main= titulo)
+  autoplot( projection, main= title)
 }
 
 ts_projection(data, star.date, frequency , interval, titulo)
-ts_projection(prom_consumo_resi_22_11$consumo_por_residencia, c(1999,07), 12, 5, "test") #sustituir. 
